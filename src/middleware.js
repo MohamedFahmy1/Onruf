161f835bc42c402d6bb1f0b282010b56ca28019a
Mohamed Fahmy <engmohamedmahmoud1997@gmail.com>
2023-10-25 00:27:37 +0300

    fixing middleware

--- src/middleware.js
+++ src/middleware.js
@@ -3,8 +3,8 @@ import { NextResponse } from "next/server"
 export default function middleware(req) {
   let url = req.url
   if (url.includes("businessAccountId") && url.includes("ProviderId") && url.includes("Token")) {
-    let parsedUrl = new URL(url, "http://localhost:3002/")
-    // let parsedUrl = new URL(url, "https://onruf.vercel.app/")
+    // let parsedUrl = new URL(url, "http://localhost:3002/")
+    let parsedUrl = new URL(url, "https://onruf.vercel.app/")
     let params = new URLSearchParams(parsedUrl.search)
     let businessAccountId = params.get("businessAccountId")
     let providerId = params.get("ProviderId")
@@ -21,8 +21,8 @@ export default function middleware(req) {
       cookies.push(`Token=${token}; Path=/;`)
     }
 
-    const response = NextResponse.redirect("http://localhost:3002/en")
-    // const response = NextResponse.redirect("https://onruf.vercel.app/en")
+    // const response = NextResponse.redirect("http://localhost:3002/en")
+    const response = NextResponse.redirect("https://onruf.vercel.app/en")
     for (let cookie of cookies) {
       response.headers.append("Set-Cookie", cookie)
     }
