d3fec5e7737cabbbeaf07ca10db9a65433c14f92
Mohamed Fahmy <engmohamedmahmoud1997@gmail.com>
2023-11-02 00:10:47 +0200

    fixing pakat  section

--- src/middleware.js
+++ src/middleware.js
@@ -6,8 +6,8 @@ export default function middleware(req) {
     return NextResponse.redirect("http://localhost:3002/en/404")
   }
   if (url.includes("businessAccountId") && url.includes("ProviderId") && url.includes("Token")) {
-    let parsedUrl = new URL(url, "http://localhost:3002/")
-    // let parsedUrl = new URL(url, "https://onruf.vercel.app/")
+    // let parsedUrl = new URL(url, "http://localhost:3002/")
+    let parsedUrl = new URL(url, "https://onruf.vercel.app/")
     let params = new URLSearchParams(parsedUrl.search)
     let businessAccountId = params.get("businessAccountId")
     let providerId = params.get("ProviderId")
@@ -22,8 +22,8 @@ export default function middleware(req) {
     if (token) {
       cookies.push(`Token=${token}; Path=/;`)
     }
-    const response = NextResponse.redirect("http://localhost:3002/en")
-    // const response = NextResponse.redirect("https://onruf.vercel.app/en")
+    // const response = NextResponse.redirect("http://localhost:3002/en")
+    const response = NextResponse.redirect("https://onruf.vercel.app/en")
     for (let cookie of cookies) {
       response.headers.append("Set-Cookie", cookie)
     }
