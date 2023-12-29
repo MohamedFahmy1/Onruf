0e5679afe5b6dfea2b1f2aa7c1405762437f4a5a
Mohamed Fahmy <engmohamedmahmoud1997@gmail.com>
2023-12-29 16:54:11 +0200

    removing unused css

--- postcss.config.js
+++ postcss.config.js
@@ -13,15 +13,13 @@ module.exports = {
         },
       },
     ],
-    process.env.NODE_ENV === "production"
-      ? [
-          "@fullhuman/postcss-purgecss",
-          {
-            content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-            defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-            safelist: ["html", "body"],
-          },
-        ]
-      : undefined,
-  ].filter(Boolean),
+    [
+      "@fullhuman/postcss-purgecss",
+      {
+        content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: ["html", "body"],
+      },
+    ],
+  ],
 }
