c6decc211338be0521a6ba6a4cf3687fd53f13f5
Mohamed Fahmy <engmohamedmahmoud1997@gmail.com>
2023-12-29 16:15:02 +0200

    removing unused css

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-}
-
 module.exports = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    domains: ["onrufwebsite6-001-site1.htempurl.com", "onruf.vercel.app"],
-  },
-  // distDir: "build",
-  nextConfig,
-  i18n: {
-    locales: ["ar", "en"],
-    defaultLocale: "ar",
-  },
-  env: {
-    REACT_APP_API_URL: "https://onrufwebsite6-001-site1.htempurl.com/api/v1",
-    MAP_API_KEY: "AIzaSyBB0w_4kUGHr54kvjKNBK_eaUo1tFuLoPU",
-  },
   plugins: [
     "postcss-flexbugs-fixes",
     [
