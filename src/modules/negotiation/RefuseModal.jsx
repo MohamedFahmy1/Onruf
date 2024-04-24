70a0820052036190ee8ae9b452979ad5f46b454a
Mohamed Fahmy <mohamedfahmy.webdeveloper@gmail.com>
2024-04-24 20:06:38 +0200

    remove refuse reason for negotation offer

--- src/modules/negotiation/RefuseModal.jsx
+++ src/modules/negotiation/RefuseModal.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react"
 import { Modal } from "react-bootstrap"
 import t from "../../translations.json"
 import { pathOr } from "ramda"
@@ -8,20 +7,20 @@ import axios from "axios"
 
 const RefuseModal = ({ refuseModal, setRefuseModal, offerId, productId, getOffers }) => {
   const { locale } = useRouter()
-  const [refuseReason, setRefuseReason] = useState()
+  // const [refuseReason, setRefuseReason] = useState()
   const refuseOffer = async () => {
-    if (refuseReason) {
-      try {
-        await axios.post(
-          `/AcceptRejectOffer?offerId=${offerId}&productId=${productId}&acceptOffer=${false}&refuseReason=${refuseReason}&OfferExpireHours=${0}`,
-        )
-        toast.success(locale === "en" ? "Offer rejected Successfully!" : "تم رفض العرض ")
-        setRefuseModal(false)
-        getOffers()
-      } catch (error) {
-        toast.error(error.response.data.message)
-      }
-    } else toast.error(pathOr("", [locale, "negotiation", "please_add_reason"], t))
+    // if (refuseReason) {
+    try {
+      await axios.post(
+        `/AcceptRejectOffer?offerId=${offerId}&productId=${productId}&acceptOffer=${false}&OfferExpireHours=${0}`,
+      )
+      toast.success(locale === "en" ? "Offer rejected Successfully!" : "تم رفض العرض ")
+      setRefuseModal(false)
+      getOffers()
+    } catch (error) {
+      toast.error(error.response.data.message)
+    }
+    // } else toast.error(pathOr("", [locale, "negotiation", "please_add_reason"], t))
   }
 
   return (
@@ -31,7 +30,7 @@ const RefuseModal = ({ refuseModal, setRefuseModal, offerId, productId, getOffer
         <button type="button" className="btn-close" onClick={() => setRefuseModal(false)}></button>
       </Modal.Header>
       <Modal.Body>
-        <div className="form-group">
+        {/* <div className="form-group">
           <h5 className="disc-header" style={{ textAlign: locale === "en" ? "left" : "right" }}>
             {pathOr("", [locale, "negotiation", "please_add_reason"], t)}
           </h5>
@@ -43,7 +42,7 @@ const RefuseModal = ({ refuseModal, setRefuseModal, offerId, productId, getOffer
             placeholder={pathOr("", [locale, "negotiation", "write_reject_reason"], t)}
             onChange={(e) => setRefuseReason(e.target.value)}
           />
-        </div>
+        </div> */}
       </Modal.Body>
       <Modal.Footer className="modal-footer">
         <button type="button" className="btn-main" onClick={refuseOffer}>
