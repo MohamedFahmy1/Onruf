e4983446c51d755ef3613d7e597d3434b2cd757b
Mohamed Fahmy <mohamedfahmy.webdeveloper@gmail.com>
2024-06-12 23:47:03 +0300

    fix pagination on deployed vercel app

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/:id*",
+        destination: "/:id*",
+      },
+    ]
+  },
   reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true,
